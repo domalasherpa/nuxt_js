23b53377445e29d72a11991220d1873a085eadb2
domalasherpa <domalasherpa9813@gmail.com>
2024-09-12 17:44:18 +0545

    feat: creates HeroSection.vue

---
+++ components/HeroSection.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="w-full flex justify-between p-10 border-2">
+        <div class="space-y-6 w-1/2">
+            <div class="space-y-4">
+                <p class="font-semibold">Branding | Image making</p>
+                <p class="text-4xl font-semibold">Visual Design</p>
+                <p>This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com</p>
+            </div>
+            <div>
+                <button class="bg-black text-white py-2 px-8">Contact</button>
+            </div>
+        </div>
+        <div>
+            <img src="~/assets/img/HeaderImage1.png" alt="" class="">
+        </div>
+    </div>
+</template>
