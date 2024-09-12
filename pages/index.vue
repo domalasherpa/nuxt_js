ca185440af8b6ed6820262cb5d3a9346c3444239
domalasherpa <domalasherpa9813@gmail.com>
2024-09-12 17:49:18 +0545

    feat: create index page

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="w-9/12">
+        <HeroSection/>
+        <CompanyBanner />
+        <SkillsSection />
+        <GallerySection />
+        <Testimonial />
+    </div>
+</template>
