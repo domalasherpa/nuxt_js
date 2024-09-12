26776192ebd4eee83a9286d1c8b00d835b056a8f
domalasherpa <domalasherpa9813@gmail.com>
2024-09-12 17:44:18 +0545

    feat: creates NavBar.vue

---
+++ components/NavBar.vue
@@ -0,0 +1,13 @@
+
+<template>
+    <div class="flex justify-between py-4">
+        <div>
+            <h1 @click="$router.push('/')">Logo</h1>
+        </div>
+        <ul class="flex space-x-4">
+            <li><NuxtLink to="/about" >About</NuxtLink></li>
+            <li><NuxtLink to="/work" >Work</NuxtLink></li>
+            <li><NuxtLink to="/contact" >Contact</NuxtLink></li>
+        </ul>
+    </div>
+</template>
