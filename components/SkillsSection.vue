9931bd9b66c9aa610c3dd2d11c659215a42654a1
domalasherpa <domalasherpa9813@gmail.com>
2024-09-12 17:44:18 +0545

    feat: creates SkillsSection.vue

---
+++ components/SkillsSection.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="flex py-10 lg:px-10 space-x-4 mt-2 text-center justify-center text-sm">
+        <div class="border-2 p-4 space-y-6">
+            <div class="flex justify-center">
+                <img src="~/assets/img/pd.png" alt="" class="object-center">
+            </div>
+            <div class="space-y-2">
+                <h1 class="text-lg font-semibold">Product Design</h1>
+                <p>This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com</p>
+            </div>
+        </div>
+        <div class="border-2 p-4 space-y-6">
+            <div class="flex justify-center">
+                <img src="~/assets/img/vd.png" alt="">
+            </div>
+            <div class="space-y-2">
+                <h1 class="text-lg font-semibold">Visual Design</h1>
+                <p>This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com</p>
+            </div>
+        </div>
+        <div class="border-2 p-4 space-y-6">
+            <div class="flex justify-center">
+                <img src="~/assets/img/ad.png" alt="">
+            </div>
+            <div class="space-y-2">
+                <h1 class="text-lg font-semibold">Art Direction</h1>
+                <p>This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com</p>
+            </div>
+        </div>
+    </div>
+</template>
