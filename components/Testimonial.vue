c8cb4e88e7548c65b7018126ddbed0817c412a17
domalasherpa <domalasherpa9813@gmail.com>
2024-09-12 17:44:18 +0545

    feat: creates Testimonial.vue

---
+++ components/Testimonial.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="grid grid-cols-3 px-10 mt-2 border-2 text-xs p-10 gap-4">
+        <div v-for="i in 6" class="space-y-4">
+            <p class="font-semibold">This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com</p>
+            <div class="flex gap-2">
+                <div>
+                    <img src="~/assets/img/client.png" alt="">
+                </div>
+                <div>
+                    <h1>Gemma Nolen</h1>
+                    <p>Google</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
