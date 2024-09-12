4fc482b67a26bbaa06bb72d80732eea7c7332695
domalasherpa <domalasherpa9813@gmail.com>
2024-09-12 17:44:18 +0545

    feat: creates CompanyBanner.vue

---
+++ components/CompanyBanner.vue
@@ -0,0 +1,23 @@
+<script setup>
+    const companyLogo = [
+        'google.png',
+        'nike.png',
+        'samsung.png',
+        'apple.png',
+        'adidas.png'
+    ]
+    const images = import.meta.glob('../assets/img/*.png', { eager: true });
+
+    const getSrc = (fileName) => {
+    // Construct the path and return the URL
+        return images[`../assets/img/${fileName}`]?.default || '';
+    };
+</script>
+
+<template>
+    <div class="flex items-center justify-between mt-2 border-2 p-4 px-10">
+        <div v-for="(cl, index) in companyLogo" :key="index">
+            <img :src="getSrc(cl)" alt="">
+        </div>
+    </div>
+</template>
